fb2fc5b407edd8edc8f169d8d08b33e28fe308ab
QEDAiiiS <mahmoudqedais306@gmail.com>
2026-05-13 10:29:32 +0300

    Fix axios url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  // baseURL: "http://localhost:5001/api",
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001",
+  baseURL: "https://rayg-app-backend-production.up.railway.app/api",
   withCredentials: true,
 });
